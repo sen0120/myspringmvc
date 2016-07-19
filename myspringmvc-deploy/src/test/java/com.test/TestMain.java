b45a3e650035ad237b508ae2180523a98890963b
sen0120 <363981162@qq.com>
2016-07-19 16:39:37 +0800

    新增新增TestMain

--- myspringmvc-deploy/src/test/java/com.test/TestMain.java
+++ myspringmvc-deploy/src/test/java/com.test/TestMain.java
@@ -1,7 +1,19 @@
 package com.test;
 
+import com.sun.tools.corba.se.idl.Noop;
+import org.springframework.cglib.proxy.*;
+
+import java.io.*;
+import java.lang.reflect.Method;
+import java.util.Properties;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * Created by fanyun on 16/7/12.
  */
 public class TestMain {
+    public static void main(String[] args) throws IOException {
+
+    }
 }
