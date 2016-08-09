733c9ecacf98a73a9da54e5e17fd1f900d6c1fe6
sen0120 <363981162@qq.com>
2016-08-09 19:50:29 +0800

    lucence测试类与elasticsearch测试类

---
+++ myspringmvc-biz/src/main/java/com/biz/test/search/Test.java
@@ -0,0 +1,18 @@
+package com.biz.test.search;
+
+import org.elasticsearch.client.Client;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+
+/**
+ * Created by fanyun on 16/8/1.
+ */
+public class Test {
+    public static void main(String[] args) {
+//        Settings settings = ImmutableSettings.settingsBuilder().put("client.transport.sniff", true).put("cluster.name", "name of node").build();
+//        Client client = new TransportClient(settings).addTransportAddress(new InetSocketTransportAddress("ip of server", 9300));
+        Object a = "";
+        System.out.println((String)a);
+    }
+}
