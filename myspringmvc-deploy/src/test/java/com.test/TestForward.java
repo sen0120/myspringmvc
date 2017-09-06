1f2aa00c4e5fe9ccdca909d7dd72b4994a3be155
凡云 <363981162@qq.com>
2017-09-06 11:38:54 +0800

    test

--- myspringmvc-deploy/src/test/java/com.test/TestForward.java
+++ myspringmvc-deploy/src/test/java/com.test/TestForward.java
@@ -2,13 +2,11 @@ package com.test;
 
 public class TestForward {
     public static void main(String[] args) {
-        //master
         System.out.println("master1");
         System.out.println(1);
         //master 2
         System.out.println(2);
         System.out.println(3);
         System.out.println(4);
-        //branch
     }
 }
