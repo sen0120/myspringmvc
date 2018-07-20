1a9443008c4b975925eeda5b377de287988484bd
凡云 <363981162@qq.com>
2018-07-20 11:13:59 +0800

    分布式锁

--- myspringmvc-deploy/src/test/java/com/test/concurrent/ReentractLockTest.java
+++ myspringmvc-deploy/src/test/java/com/test/concurrent/ReentractLockTest.java
@@ -32,8 +32,8 @@ public class ReentractLockTest implements Runnable {
             @Override
             public void run() {
                 writeLock();
-                writeLock();
-//                writeUnlock();
+//                writeLock();
+                writeUnlock();
             }
         }).start();
 
